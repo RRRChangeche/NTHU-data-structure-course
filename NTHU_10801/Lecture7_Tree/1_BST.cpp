4d08226ccc534b04b54b01dca6b06aeb4769fc22
RRRChangeche <r03522631@ntu.edu.tw>
2022-04-12 17:11:42 +0800

    update Lecture7 BSTree, add Insert and delete function

--- NTHU_10801/Lecture7_Tree/1_BST.cpp
+++ NTHU_10801/Lecture7_Tree/1_BST.cpp
@@ -4,144 +4,229 @@
 using namespace std;
 
 // forward declaration of Binary Search Tree
-template <class T, class K, class E> class BSTree;
-// template <class T> class BSTree; 
+template <class K, class E> class BSTree;
 
-
-// declare key-value pair
+// declare TreeNode
 template <class K, class E>
-class Pair{
+class TreeNode{
+friend class BSTree<K, E>;
 public: 
-    Pair() {}
-    Pair(K k, E elem) {
+    // constructor
+    TreeNode(K k, E elem, TreeNode<K, E>* left=NULL, TreeNode<K, E>* right=NULL){
         key = k;
         element = elem;
+        leftChild = left;
+        rightChild = right;
+        leftSize = GetLeftSize(this);
     }
+    TreeNode(TreeNode<K,E>* node){ 
+        key = node->key;
+        element = node->element;
+        leftChild = node->leftChild;
+        rightChild = node->rightChild;
+        leftSize = GetLeftSize(node);
+    }
+    // ~TreeNode() { delete this; }
+
+    int GetLeftSize(TreeNode<K,E>* node){
+        int count = 0;
+        while (node){
+            node = node->leftChild;
+            count++;
+        }
+        return count;
+    };
+
+    int GetLeftSize(){
+        int count = 0;
+        TreeNode<K,E>* node = this;
+        while (node){
+            node = node->leftChild;
+            count++;
+        }
+        return count;
+    };
+ 
+    E GetElement(){ return element;}
 
 private:
     K key;
     E element;
-};
-
-// declare TreeNode
-template <class T>
-class TreeNode{
-// friend class BSTree<T, K, E>;
-public: 
-    // constructor
-    TreeNode<T>(T value, TreeNode<T>* left=NULL, TreeNode<T>* right=NULL){
-        data = value;
-        leftChild = left;
-        rightChild = right;
-    }
-
-private:
-    T data;
-    TreeNode<T>* leftChild;
-    TreeNode<T>* rightChild;
+    TreeNode<K, E>* leftChild;
+    TreeNode<K, E>* rightChild;
     int leftSize;
 };
 
 // declare Binary Search Tree
-template <class T, class K, class E>
+template <class K, class E>
 class BSTree{
 public:
     BSTree() {root=NULL;}
-    BSTree(TreeNode<T>* node) {root=node;}
+    BSTree(TreeNode<K,E>* node) {root=node;}
     ~BSTree() {}
 
-    T* Get(const K& k);
-    T* GetByRecursive(const K& k);
+    TreeNode<K,E>* Get(const K& k);
+    TreeNode<K,E>* GetByRecursive(const K& k);
 private:
-    T* GetByRecursive(TreeNode<T>* node, const K& k);
+    TreeNode<K,E>* GetByRecursive(TreeNode<K,E>* node, const K& k);
 public:
-    T* GetByRank(int r);
-    void Insert(TreeNode<T>* node);
+    TreeNode<K,E>* GetByRank(int r);
+    void Insert(TreeNode<K,E>* node);
+    void Delete(const K& k);
 
 private:
-    TreeNode<T>* root;
+    TreeNode<K,E>* root;
 };
 
 
 // Interative search
-template <class T, class K, class E>
-T* BSTree<T, K, E>::Get(const K& key){
-    TreeNode<T>* currentNode = root;
+template <class K, class E>
+TreeNode<K,E>* BSTree<K,E>::Get(const K& key){
+    TreeNode<K,E>* currentNode = root;
     while(currentNode){
-        if (key < currentNode->data.key) currentNode = currentNode->leftChild;
-        else if (key > currentNode->data.key) currentNode = currentNode->rightChild;
-        else return & currentNode->data;
+        if (key < currentNode->key) currentNode = currentNode->leftChild;
+        else if (key > currentNode->key) currentNode = currentNode->rightChild;
+        else return currentNode;
     }
 
     return NULL;     // no match found
 }
 
 // Recursive search
-template <class T, class K, class E>
-T* BSTree<T, K, E>::GetByRecursive(const K& key){
+template <class K, class E>
+TreeNode<K,E>* BSTree<K,E>::GetByRecursive(const K& key){
     return GetByRecursive(root, key);
 }
 
-template <class T, class K, class E>
-T* GetByRecursive(TreeNode<T>* node, const K& k){
+template <class K, class E>
+TreeNode<K,E>* GetByRecursive(TreeNode<K,E>* node, const K& k){
     if (!node) {
         cout << "Node not found!" << endl;
         return 0;
     } 
-    if (k < node->data) GetByRecursive(node->leftChild, k);
-    if (k > node->data) GetByRecursive(node->rightChild, k);
-    return &node->data;
+    if (k < node->key) GetByRecursive(node->leftChild, k);
+    if (k > node->key) GetByRecursive(node->rightChild, k);
+    return node;
 }
 
 // Search by rank
-template <class T, class K, class E>
-T* BSTree<T, K, E>::GetByRank(int r){
+template <class K, class E>
+TreeNode<K,E>* BSTree<K,E>::GetByRank(int r){
     // Search BST for the rth smallest pair
-    TreeNode<T>* currentNode = root;
+    TreeNode<K,E>* currentNode = root;
     while(currentNode){
-        if(r < currentNode->leftSize) 
+        if(r < currentNode->GetLeftSize()) 
             currentNode = currentNode->leftChild;
-        else if(r > currentNode->leftSize) { 
-            r -= currentNode->leftSize;
-            currentNode = currentNode->rigthChild;
+        else if(r > currentNode->GetLeftSize()) { 
+            r -= currentNode->GetLeftSize();
+            currentNode = currentNode->rightChild;
         }
-        else return &currentNode->data;
+        else return currentNode;
     }
     return 0;
 }
 
-template <class T, class K, class E>
-void BSTree<T, K, E>::Insert(TreeNode<T>* node){
-    TreeNode<T>* p = root, *pp=0;
+template <class K, class E>
+// template <class T>
+void BSTree<K,E>::Insert(TreeNode<K,E>* node){
+    TreeNode<K,E>* p = root, *pp=0;
+    // traversal BST and find if key exists
+    // if not, store the search terminate node's parent as pp
     while(p){
         pp = p;
-        if (node->data.key < p->data.key) p = p->leftChild;
-        else if (node->data.key > p->data.key) p = p->rightChild;
-        else {p.data.element = node->data.element; return;} // found same key, update the value of element
+        if (node->key < p->key) p = p->leftChild;
+        else if (node->key > p->key) p = p->rightChild;
+        else {p->element = node->element; return;} // found same key, update the value of element
     }
+
     // perform the insertion
-    p = new Pair<K,E>(node->data);
+    // set p as pp's leftChild or rightChild
+    p = new TreeNode(node);
     if (root)   // if tree is not empty
     {
-        if (node->data.key < pp->data.key) pp->leftChild = p;
+        if (node->key < pp->key) pp->leftChild = p;
         else pp->rightChild = p;
     }
     else root=p;
 }
 
+template<class K, class E>
+void BSTree<K,E>::Delete(const K& k){
+    // find node to be delete, and pNode is it's parent node
+    TreeNode<K,E> *node=root, *pNode = 0;
+    while(node){
+        if (k == node->key) break;  // found node with key
+        pNode = node;
+        if (k < node->key) node = node->leftChild;
+        else if (k > node->key) node = node->rightChild;  
+    }
+
+    if (node){
+        cout << "delete node with key=" <<node->key  << " in BST"<< endl;
+        // node with two children (non-leaf node)
+        if (node->leftChild && node->rightChild){       
+            // delete node and replace by smallest node in right subtree
+            TreeNode<K,E>* sNode = node->rightChild;
+            while(sNode->leftChild) sNode = sNode->leftChild;
+            delete node;
+            node = sNode;
+            delete sNode;
+            sNode = NULL;
+        }
+        else{
+        // node with no child (leaf node)
+            if (!node->leftChild && !node->rightChild){ 
+                if (k < pNode->key) pNode->leftChild = NULL;
+                else pNode->rightChild = NULL;
+            }
+        // node with one child (non-leaf node)
+            else{         
+                // if node has leftChild                              
+                if (node->leftChild){                   
+                    if (k < pNode->key) pNode->leftChild = node->leftChild;     // subtree is at pNode's leftside
+                    else pNode->rightChild = node->leftChild;                   // subtree is at pNode's rightside
+                }
+                // if node has rightChild
+                else if (node->rightChild){             
+                    if (k < pNode->key) pNode->leftChild = node->rightChild;    // subtree is at pNode's leftside
+                    else pNode->rightChild = node->rightChild;                  // subtree is at pNode's rightside
+                }
+            }
+            delete node;
+            node = NULL;
+        }
+    }
+    else {                                              // key not found 
+        cout << "key not found in BST" << endl; 
+    }   
+}
+
 
 int main () {
     // TreeNode instantiation
-    typedef TreeNode<Pair<int, const char*>> TNodePair;
-    Pair<int, const char*> pairA(9, "A");
-    Pair<int, const char*> pairB(8, "B");
-    TNodePair* nodeA = new TreeNode(pairA); TNodePair* nodeB = new TreeNode(pairB);
-    // TNodePair* nodeC = new TreeNode(new Pair(7, "C")); TNodePair* nodeD = new TreeNode(new Pair(6, "D")); 
-    // TNodePair* nodeE = new TreeNode(new Pair(5, "E")); TNodePair* nodeF = new TreeNode(new Pair(4, "F")); 
-    // TNodePair* nodeG = new TreeNode(new Pair(3, "G")); TNodePair* nodeH = new TreeNode(new Pair(2, "H")); 
-    // TNodePair* nodeI = new TreeNode(new Pair(1, "I"));
+    typedef TreeNode<int, const char*> BSTNode;
+    BSTNode* nodeA = new BSTNode(9, "A"); BSTNode* nodeB = new BSTNode(8, "B");
+    BSTNode* nodeC = new BSTNode(7, "C"); BSTNode* nodeD = new BSTNode(6, "D"); 
+    BSTNode* nodeE = new BSTNode(5, "E"); BSTNode* nodeF = new BSTNode(4, "F"); 
+    BSTNode* nodeG = new BSTNode(3, "G"); BSTNode* nodeH = new BSTNode(2, "H"); 
+    BSTNode* nodeI = new BSTNode(1, "I");
 
-    BSTree<Pair<int, const char*>,int,const char*> T;
+    BSTree<int,const char*> T;
     T.Insert(nodeA);
+    T.Insert(nodeB);
+    T.Insert(nodeC);
+    T.Insert(nodeD);
+    T.Insert(nodeE);
+    T.Insert(nodeF);
+    T.Insert(nodeG);
+    T.Insert(nodeH);
+    T.Insert(nodeI);
+    cout << T.Get(9)->GetElement() << endl;
+    cout << T.Get(8)->GetElement() << endl;
+    cout << T.GetByRank(2)->GetElement() << endl;
+
+    T.Delete(2);
+    cout << T.GetByRank(2)->GetElement() << endl;
+    
     return 0;
 }
